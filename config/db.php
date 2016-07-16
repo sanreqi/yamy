bc781ecdc99520bb38240ab9652289a4d79d0323
sanreqi <358779016@qq.com>
2016-07-16 11:29:55 +0800

    d

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yamy',
+    'username' => 'root',
+    'password' => 'abc@12q13',
+    'charset' => 'utf8',
+];
