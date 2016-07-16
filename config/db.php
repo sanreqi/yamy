2f7651215f2708aa7a2239008bb10349753920d6
sanreqi <358779016@qq.com>
2016-07-16 11:32:04 +0800

    modify

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yamy',
     'username' => 'root',
-    'password' => 'abc@12q13',
+    'password' => 'abc@12iq13',
     'charset' => 'utf8',
 ];
