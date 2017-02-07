71584a8a15b356a2bb171dda7ef69ac26282d031
srq <358779016@qq.com>
2017-02-07 16:20:23 +0800

    modify

--- modules/auth/views/user/login.php
+++ modules/auth/views/user/login.php
@@ -33,19 +33,19 @@
         </div>
         <div class="footer">
             <button type="submit" class="btn bg-olive btn-block">立即登录</button>
-            <p><a href="javascript:void(0)">忘记密码</a></p>
-            <a href="javascript:void(0)" class="text-center">注册</a>
+<!--            <p><a href="javascript:void(0)">忘记密码</a></p>-->
+<!--            <a href="javascript:void(0)" class="text-center">注册</a>-->
         </div>
     </form>
 
-    <div class="margin text-center">
-        <span>社区活动</span>
-        <br/>
-        <button class="btn bg-light-blue btn-circle"><i class="fa fa-facebook"></i></button>
-        <button class="btn bg-aqua btn-circle"><i class="fa fa-twitter"></i></button>
-        <button class="btn bg-red btn-circle"><i class="fa fa-google-plus"></i></button>
-
-    </div>
+<!--    <div class="margin text-center">-->
+<!--        <span>社区活动</span>-->
+<!--        <br/>-->
+<!--        <button class="btn bg-light-blue btn-circle"><i class="fa fa-facebook"></i></button>-->
+<!--        <button class="btn bg-aqua btn-circle"><i class="fa fa-twitter"></i></button>-->
+<!--        <button class="btn bg-red btn-circle"><i class="fa fa-google-plus"></i></button>-->
+<!---->
+<!--    </div>-->
 </div>
 
 </body>
